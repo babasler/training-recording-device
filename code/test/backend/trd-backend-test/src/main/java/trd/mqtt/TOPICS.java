b340b2a346fbd61c474198158f0ac49426873a9c
Bastian Basler <bastian.basler2002@gmail.com>
2025-03-08 13:27:37 +0100

    delete Code

--- code/test/backend/trd-backend-test/src/main/java/trd/mqtt/TOPICS.java
+++
@@ -1,6 +0,0 @@
-package trd.mqtt;
-
-public enum TOPICS
-{
-
-}
--- code/test/backend/trd-backend-test/src/main/java/trd/mqtt/broker/Main.java
+++
@@ -1,9 +0,0 @@
-package trd.mqtt.broker;
-
-public class Main
-{
-     public static void main(String[] args) {
-        broker b = new broker();
-        b.start();
-    }
-}
