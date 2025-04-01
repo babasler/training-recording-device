39f78da872532bc285b4aaaeb48b439c64152835
Bastian Basler <bastian.basler2002@gmail.com>
2025-04-01 17:30:36 +0200

    new Sidebar

--- code/test/frontend/src/app/components/sidebar/sidebar.component.ts
+++ code/test/frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-sidebar',
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+    selector: 'sidebar',
+    templateUrl: './sidebar.component.html',
+    standalone: true,
+    imports: [DrawerModule, ButtonModule]
 })
 export class SidebarComponent {
-
+    visible: boolean = false;
 }
