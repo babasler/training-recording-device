0a2c77108ebe34ede1219ef9371a6831d7ac09df
Bastian Basler <bastian.basler2002@gmail.com>
2025-04-02 18:35:13 +0200

    add moving sidebar

--- code/test/frontend/src/app/components/sidebar/sidebar.component.ts
+++ code/test/frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DrawerModule } from 'primeng/drawer';
 import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClass } from 'primeng/styleclass';
+import { Drawer } from 'primeng/drawer';
 
 @Component({
     selector: 'sidebar',
     templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss'],
     standalone: true,
-    imports: [DrawerModule, ButtonModule]
+    imports: [DrawerModule, ButtonModule, AvatarModule]
 })
 export class SidebarComponent {
+
+    closeCallback(): void {
+        this.visible = false;
+    }
     visible: boolean = false;
 }
