a821f1affc3cc77717b6d82c5af0ef0cbf3b5d4e
Bastian Basler <bastian.basler2002@gmail.com>
2025-04-02 19:35:05 +0200

    sidebar done

--- code/test/frontend/src/app/components/sidebar/sidebar.component.ts
+++ code/test/frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { DrawerModule } from 'primeng/drawer';
 import { ButtonModule } from 'primeng/button';
-import { Ripple } from 'primeng/ripple';
 import { AvatarModule } from 'primeng/avatar';
-import { StyleClass } from 'primeng/styleclass';
-import { Drawer } from 'primeng/drawer';
 
 @Component({
     selector: 'sidebar',
