c75b99c2cf0472ced7ade7be22768b762f879b7b
Bastian Basler <bastian.basler2002@gmail.com>
2025-04-07 21:45:26 +0200

    refactor Dashboardpage

--- code/test/frontend/src/app/components/dashboard/chart/chart.component.spec.ts
+++ code/test/frontend/src/app/components/dashboard/chart/chart.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LastActivityComponent } from './last-activity.component';
+import { LastActivityComponent } from './chart.component';
 
 describe('LastActivityComponent', () => {
   let component: LastActivityComponent;
--- code/test/frontend/src/app/components/dashboard/dashboard-component.component.ts
+++ code/test/frontend/src/app/components/dashboard/dashboard-component.component.ts
@@ -1,24 +1,62 @@
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { ButtonModule } from 'primeng/button';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { ChangeDetectorRef, Component, effect, inject, OnInit, PLATFORM_ID } from '@angular/core';
-//import { AppConfigService } from '../../services/appconfigservice';
+import { CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
 import { ChartModule } from 'primeng/chart';
 import { WeightChartComponent } from "./weight-chart/weight-chart.component";
 import { VolumeChartComponent } from "./volume-chart/volume-chart.component";
 import { SetsChartComponent } from "./sets-chart/sets-chart.component";
 import { SelectPeriodeBarComponent } from "./select-periode-bar/select-periode-bar.component";
-import { LastActivityComponent } from "./last-activity/last-activity.component";
-import { NextActivityComponent } from "./next-activity/next-activity.component";
+import { LastActivityComponent } from "./chart/chart.component";
 
+interface chartData{
+  header: string,
+  image: string,
+  description: string
+}
 
 @Component({
   selector: 'app-dashboard-component',
-  imports: [SidebarComponent, ButtonModule, CommonModule, ChartModule, WeightChartComponent, VolumeChartComponent, SetsChartComponent, SelectPeriodeBarComponent, LastActivityComponent, NextActivityComponent],
+  imports: [SidebarComponent, ButtonModule, CommonModule, ChartModule, WeightChartComponent, VolumeChartComponent, SetsChartComponent, SelectPeriodeBarComponent, LastActivityComponent],
   templateUrl: './dashboard-component.component.html',
   styleUrl: './dashboard-component.component.scss'
 })
 export class DashboardComponent{
+
   currentDiv: string = 'div1'; // Startwert
   userName: string = 'Max Mustermann';
+
+  //chartsData: Observable<chartData[]> = of([]);
+  chartsData: Array<chartData> = [];
+
+
+  async ngOnInit() {
+    this.chartsData = this.getChartsData();
+  }
+
+  getChartsData(): chartData[] {
+    // Replace 'your-api-endpoint-url' with the actual API endpoint if needed
+    return [
+      {
+        header: 'Nächstes Training',
+        image: './hantel-2.png',
+        description: 'Ganzkörpertraining'
+      },
+      {
+        header: 'Letztes Training',
+        image: './hantel-2.png',
+        description: 'Ganzkörpertraining'
+      },
+      {
+        header: 'Trainingsdauer',
+        image: './repeat.png',
+        description: 'Durchschnittlich X min'
+      },
+      {
+        header: 'Puls',
+        image: './cardiogram.png',
+        description: 'Durchnittlich X bpm'
+      }
+    ];
+  }
 }
